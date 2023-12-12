c73df14a0e6592a2e699bb15f2de022000399182
Philipp Stephani <p.stephani2@gmail.com>
2023-12-12 19:29:07 +0100

    Add a mode for REPO.bazel files.
    See https://docs.google.com/document/d/1rS-B3d_sfZFY2AcSwIq2ibQc7X1lgeycddLJqjYKgOA/comment.

--- test.el
+++ test.el
@@ -1446,6 +1446,7 @@ Process buildifier exited abnormally with code 1
                      ("WORKSPACE" bazel-workspace-mode)
                      ("WORKSPACE.bazel" bazel-workspace-mode)
                      ("MODULE.bazel" bazel-module-mode)
+                     ("REPO.bazel" bazel-repo-mode)
                      ("def.bzl" bazel-starlark-mode)
                      (".bazelrc" bazelrc-mode)
                      (".bazelignore" bazelignore-mode)))
