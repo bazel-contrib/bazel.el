a8fcd232d262740120880825cffffba0eebf3feb
Philipp Stephani <p.stephani2@gmail.com>
2026-08-19 14:48:39 +0200

    Update reference

--- bazel.el
+++ bazel.el
@@ -685,9 +685,8 @@ return its name.  See URL
   (setq-local comment-end "")
   (setq-local comment-use-syntax t)
   ;; In .bazelignore files, comments need to cover whole lines,
-  ;; cf. https://github.com/bazelbuild/bazel/blob/09c621e4cf5b968f4c6cdf905ab142d5961f9ddc/src/main/java/com/google/devtools/build/lib/skyframe/IgnoredPackagePrefixesFunction.java#L123.
-  ;; We need to install a custom ‘syntax-propertize-function’ to deal with
-  ;; this.
+  ;; cf. https://github.com/bazelbuild/bazel/blob/9.2.0/src/main/java/com/google/devtools/build/lib/skyframe/IgnoredSubdirectoriesFunction.java#L208
+  ;; We need to install a custom ‘syntax-propertize-function’ to deal with this.
   (setq-local syntax-propertize-function #'bazelignore--syntax-propertize)
   ;; Ensure that ‘syntax-propertize’ always looks at entire lines.
   (add-hook 'syntax-propertize-extend-region-functions
